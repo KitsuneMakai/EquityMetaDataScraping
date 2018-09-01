afddc6f97b295807c0240d7357c966e043989ff7
thesouldemon <noah.kalinowski@gmail.com>
2018-09-01 16:36:56 -0400

    Rewrite using request, tickers in file

--- dividend.py
+++ dividend.py
@@ -1,41 +1,36 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+import requests
 from bs4 import BeautifulSoup
-import pandas as pd
+import os
 
-opts = Options()
-opts.add_argument("user-agent=Googlebot/2.1(+http://www.googlebot.com/bot.html)")
+# load the tickers from the ticker file
+ticker_list = [x.strip() for x in open("tickers.txt", "r").readlines()]
 
-ticker_list = ["AMZN","BDK","BA","CAT","CHTR","CMCSA","COST","DHR","EK","EBAY","F","GD","GM","HON",
-               "JCI","LB","LMT","LOW","MCD","RTN","ROK","SPG","SBUX","TGT","DIS","TWX","FOXA","FOX",
-               "WMT","WY","MO","BUD","AVP","BKNG","CPB","CBS","CL","HNZ","HSH","HD","KHC","MDLZ","NKE",
-               "PEP","PM","KO","APC","APA","BHI","CVX","COP","DVN","DUK","EP","EMR","XOM","GE","HAL","NOV",
-               "SLB","AIG","ALL","AXP","BAC","BRK.B","BLK","BK","COF","C","GS","HIG","JPM","LEH","MA","MER",
-               "MET","MS","NYX","PYPL","V","WB","WFC","RF","USB","SGP","UNH","WBA","WYE","MMM","ABT",
-               "AGN","AMGN","BAX","BIIB","BMY","CELG","CI","COV","CVS","LLY","GILD","JNJ","MDT","MRK","PFE",
-               "PG","NSC","AES","BNI","DWDP","DD","FDX","FCX","MON","NSM","UNP","UPS","IP","ACN",
-               "GOOG","AAPL","CSCO","CSC","DELL","EMC","FB","HPQ","IBM","MSFT","ORCL","QCOM","TXN","INTC",
-               "XRX","AA","ATI","VZ","AEP","T","ETR","EXC","KMI","NEE","SO","S","WMB"]
-print("Number of equities: ",len(ticker_list))
+print("Number of equities: ", len(ticker_list))
 
 for ticker in ticker_list:
     print(ticker)
-    url = "https://www.dividendchannel.com/history/?symbol="+ticker
-    driver = webdriver.Chrome(chrome_options=opts,executable_path="F:\PythonApps\ChromeDriver\chromedriver.exe")
-    driver.get(url)
-    soup = BeautifulSoup(driver.page_source,"lxml")
-    driver.quit()
+    # URL to get dividend data
+    url = "https://www.tickertech.net/bnkinvest/cgi/?n=2&ticker=" + ticker + "&js=on&a=historical&w=dividends2"
+    req = requests.get(url)
+    soup = BeautifulSoup(req.content, "lxml")
 
-    dividend_rows = soup.select("div#divvytable")[0].find_all("tr")
+    # find the one table we are looking for with dividend data, and get all table rows from that table
+    dividend_rows = soup.find("table", attrs={"bgcolor": "#EEEEEE"}).find_all("tr")
 
-    output_filename = "F:\System\PVWAVE\Scraped_Data\EquityDividends\%s.csv"%ticker
-    output_file = open(output_filename,'w',newline='')
-                            
+    # set the output file to be `$ticker.csv` in the folder `dividends`
+    output_filename = "dividends\{0}.csv".format(ticker)
+    # Check if the folder exists, if not make it
+    os.makedirs(os.path.dirname(output_filename), exist_ok=True)
+    # make/open the ticker file
+    output_file = open(output_filename, 'w')
+
     for row in dividend_rows:
-        columns= list(row.stripped_strings)
-        if len(columns) != 2:
-            continue
-        output_file.write("{}, {} \n".format(columns[0],columns[1]))
-    
-    output_file.close()   
+        # extract all the strings from the row
+        columns = list(row.stripped_strings)
+        # remove the lingering javascript rows
+        columns = [x for x in columns if 'allow' not in x]
+        # if there are only 2 columns (date, ratio), the data is correct and we can write it
+        if len(columns) == 2:
+            output_file.write("{0}, {1} \n".format(columns[0], columns[1]))
 
+    output_file.close()
--- split.py
+++ split.py
@@ -1,41 +1,43 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+import requests
 from bs4 import BeautifulSoup
-import pandas as pd
+import os
 
-opts = Options()
-opts.add_argument("user-agent=Googlebot/2.1(+http://www.googlebot.com/bot.html)")
+# load the tickers from the ticker file
+ticker_list = [x.strip() for x in open("tickers.txt", "r").readlines()]
 
-ticker_list = ["AMZN","BDK","BA","CAT","CHTR","CMCSA","COST","DHR","EK","EBAY","F","GD","GM","HON",
-               "JCI","LB","LMT","LOW","MCD","RTN","ROK","SPG","SBUX","TGT","DIS","TWX","FOXA","FOX",
-               "WMT","WY","MO","BUD","AVP","BKNG","CPB","CBS","CL","HNZ","HSH","HD","KHC","MDLZ","NKE",
-               "PEP","PM","KO","APC","APA","BHI","CVX","COP","DVN","DUK","EP","EMR","XOM","GE","HAL","NOV",
-               "SLB","AIG","ALL","AXP","BAC","BRK.B","BLK","BK","COF","C","GS","HIG","JPM","LEH","MA","MER",
-               "MET","MS","NYX","PYPL","V","WB","WFC","RF","USB","SGP","UNH","WBA","WYE","MMM","ABT",
-               "AGN","AMGN","BAX","BIIB","BMY","CELG","CI","COV","CVS","LLY","GILD","JNJ","MDT","MRK","PFE",
-               "PG","NSC","AES","BNI","DWDP","DD","FDX","FCX","MON","NSM","UNP","UPS","IP","ACN",
-               "GOOG","AAPL","CSCO","CSC","DELL","EMC","FB","HPQ","IBM","MSFT","ORCL","QCOM","TXN","INTC",
-               "XRX","AA","ATI","VZ","AEP","T","ETR","EXC","KMI","NEE","SO","S","WMB"]
-print("Number of equities: ",len(ticker_list))
+print("Number of equities: ", len(ticker_list))
 
 for ticker in ticker_list:
     print(ticker)
+    # url to get split data
     url = "https://www.stocksplithistory.com/?symbol="+ticker
-    driver = webdriver.Chrome(chrome_options=opts,executable_path="F:\PythonApps\ChromeDriver\chromedriver.exe")
-    driver.get(url)
-    soup = BeautifulSoup(driver.page_source,"lxml")
-    driver.quit()
+    req = requests.get(url)
+    soup = BeautifulSoup(req.content, "lxml")
 
-    split_rows = soup.find('table', attrs={'border': '0', 'width': '208', 'style':'font-family: Arial; font-size: 12px'}).find_all("tr")
+    # the only table with these attributes is the table with the data, so we use them to find it
+    table = soup.find('table', attrs={'border': '0', 'width': '208', 'style':'font-family: Arial; font-size: 12px'})
+    # save the `$ticker split history, table` header
+    header = table.find("td")
+    # get all the table rows; the split data
+    split_rows = table.find_all("tr")
 
-    output_filename = "F:\System\PVWAVE\Scraped_Data\EquitySplits\%s.csv"%ticker
-    output_file = open(output_filename,'w',newline='')
-                            
+    # set the output file to be `$ticker.csv` in the folder `splits`
+    output_filename = "splits\{0}.csv".format(ticker)
+    # Check if the folder exists, if not make it
+    os.makedirs(os.path.dirname(output_filename), exist_ok=True)
+    # make/open the ticker file
+    output_file = open(output_filename, 'w')
+
+    # extract the strings from the header and write it
+    columns = list(header.stripped_strings)
+    if len(columns) == 2:
+        output_file.write("{0}, {1} \n".format(columns[0], columns[1]))
+
     for row in split_rows:
-        columns= list(row.stripped_strings)
-        if len(columns) != 2:
-            continue
-        output_file.write("{}, {} \n".format(columns[0],columns[1]))
-    
-    output_file.close()   
+        # extract the strings from thr row
+        columns = list(row.stripped_strings)
+        # if the length is 2, it is in the right format (date, split), so we write it
+        if len(columns) == 2:
+            output_file.write("{0}, {1} \n".format(columns[0], columns[1]))
 
+    output_file.close()
